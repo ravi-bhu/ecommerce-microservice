10b548f127cb228f39aa9c61bc2f202e2d3300b4
Ravi Bhushan <ravibhushan122@gmail.com>
2021-08-23 20:14:38 +0530

    added eureka-server, order-service and product-service modules

---
+++ product-service/src/test/java/com/bhushan/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bhushan.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    @SuppressWarnings("java:S2699") // Body not required for this test case
+    void contextLoads() {
+    }
+
+}
